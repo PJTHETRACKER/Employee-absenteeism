aa01ea3cba71aa36f3bd7b85e3fa2b9d57bbe864
Pranjul Gupta <pranjulgupta.c@gmail.com>
2019-03-15 00:35:50 +0530

    Initial commit

---
+++ absent_spi.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import math
+from scipy.stats import chi2_contingency
+import os
+import seaborn as sns #for plotting
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
